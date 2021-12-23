304bf0caeaf0e8fad11bd554325dbe2893141f60
Katya <katsiaryna.auchynnikava@innowise-group.com>
2021-12-23 15:37:22 +0300

    initial commit

---
+++ src/test/java/com/planeticketregistration/PlaneTicketRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.planeticketregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaneTicketRegistrationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
