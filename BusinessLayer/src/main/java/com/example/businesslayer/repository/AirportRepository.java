2ccfdab6154e345dfa9423a2f78a8baa48e6e441
Katya <katsiaryna.auchynnikava@innowise-group.com>
2022-01-04 17:33:53 +0300

    Update repository

--- BusinessLayer/src/main/java/com/example/businesslayer/repository/AirportRepository.java
+++ BusinessLayer/src/main/java/com/example/businesslayer/repository/AirportRepository.java
@@ -2,7 +2,9 @@ package com.example.businesslayer.repository;
 
 import com.example.businesslayer.domain.Airport;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AirportRepository extends JpaRepository<Airport, Long> {
 
 }
--- BusinessLayer/src/main/java/com/example/businesslayer/repository/FlightRepository.java
+++ BusinessLayer/src/main/java/com/example/businesslayer/repository/FlightRepository.java
@@ -3,7 +3,9 @@ package com.example.businesslayer.repository;
 import com.example.businesslayer.domain.Airport;
 import com.example.businesslayer.domain.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface FlightRepository extends JpaRepository<Flight, Long> {
 
 }
