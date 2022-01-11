b8c976e415f9b432b44d99dc241f94106bdb91cf
Katya <katsiaryna.auchynnikava@innowise-group.com>
2022-01-11 17:20:27 +0300

    Create read by id-method for airplane controller
    Create read all-method for airplane controller

--- BusinessLayer/src/main/java/com/example/businesslayer/controller/AirportController.java
+++ BusinessLayer/src/main/java/com/example/businesslayer/controller/AirportController.java
@@ -1,5 +1,6 @@
 package com.example.businesslayer.controller;
 
+import com.example.businesslayer.domain.AirportEntity;
 import com.example.businesslayer.dto.request.AirportRequestDto;
 import com.example.businesslayer.dto.response.AirportResponseDto;
 import com.example.businesslayer.mapper.AirportEntityToAirportResponseDtoMapper;
@@ -11,10 +12,17 @@ import lombok.Setter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequiredArgsConstructor
@@ -37,6 +45,28 @@ public class AirportController {
         return new ResponseEntity<>( airportResponseDto, HttpStatus.CREATED);
 
     }
+
+    //read all airports
+    @GetMapping
+    public List<AirportResponseDto> readAll(){
+        return airportService
+                .readAll()
+                .stream()
+                .collect(ArrayList::new,
+                        ((airportResponseDtoList, airport) -> airportResponseDtoList.add(airportEntityToAirportDtoMapper.map(airport))),
+                        ArrayList::addAll);
+
+    }
+
+    //read by id
+    @GetMapping
+    public AirportResponseDto readById(@RequestParam("id") Long id){
+        return airportEntityToAirportDtoMapper.map(airportService.readById(id));
+    }
+
+    //update airport
+
+
 
 
 
