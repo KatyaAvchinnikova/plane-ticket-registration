95dfcb25575a14b915b3ab7af51c31f8c5c2fe4c
KatyaAvchinnikova <katsiaryna.auchynnikava@innowise-group>
2022-02-03 22:49:35 +0300

    Add JwtProvider

--- BusinessLayerNew/src/main/java/com/example/businesslayernew/security/jwt/JwtProvider.java
+++ BusinessLayerNew/src/main/java/com/example/businesslayernew/security/jwt/JwtProvider.java
@@ -1,6 +1,7 @@
 package com.example.businesslayernew.security.jwt;
 
 import com.example.businesslayernew.domain.User;
+import com.example.businesslayernew.exception.JwtAuthenticationException;
 import com.example.businesslayernew.security.JwtUserDetailsService;
 import com.example.businesslayernew.service.UserService;
 import io.jsonwebtoken.*;
@@ -10,9 +11,11 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 import java.time.ZoneId;
+import java.util.Base64;
 import java.util.Date;
 
 @Component
@@ -20,46 +23,54 @@ import java.util.Date;
 public class JwtProvider {
     private static final String KEY = "secret-key";
 
+    private static final long VALIDITY_IN_MILLISECONDS = 3600000;
+
     private final JwtUserDetailsService userDetailsService;
 
     private final UserService userService;
 
+    private String secret;
+
     public String getUsername(String token) {
-        Claims claims = Jwts.parser()
-                .setSigningKey(KEY)
+        return Jwts.parser().setSigningKey(secret)
                 .parseClaimsJws(token)
-                .getBody();
-        return claims.getSubject();
+                .getBody()
+                .getSubject();
     }
 
-    public String getToken(String email) {
-        Date expDate = Date.from(LocalDate.now().plusDays(14).atStartOfDay(ZoneId.systemDefault()).toInstant());
-        System.out.println(KEY);
-        return Jwts.builder()
-                .setSubject(email)
-                .setExpiration(expDate)
-                .signWith(SignatureAlgorithm.HS512, KEY)
+    public String createToken(String username) {
+        Claims claims = Jwts.claims().setSubject(username);
+
+        Date now = new Date();
+        Date validity = new Date(now.getTime() + VALIDITY_IN_MILLISECONDS);
+
+        return Jwts.builder()//
+                .setClaims(claims)//
+                .setIssuedAt(now)//
+                .setExpiration(validity)//
+                .signWith(SignatureAlgorithm.HS256, secret)//
                 .compact();
     }
 
     public boolean validateToken(String token) {
         try {
-            Jwts.parser().setSigningKey(KEY).parseClaimsJws(token);
-            return true;
-        } catch (ExpiredJwtException |
-                UnsupportedJwtException |
-                MalformedJwtException |
-                SignatureException exception) {
-            exception.printStackTrace();
-        }
+            Jws<Claims> claims = Jwts.parser().setSigningKey(secret).parseClaimsJws(token);
 
-        return false;
+            if (claims.getBody().getExpiration().before(new Date())) {
+                return false;
+            }
+
+            return true;
+
+        } catch (JwtException | IllegalArgumentException e) {
+            throw new JwtAuthenticationException("JWT token is expired or invalid");
+        }
     }
 
-    String resolveToken(HttpServletRequest request) {
-        String bearerToken = request.getHeader("Authorization");
-        if (bearerToken != null && bearerToken.startsWith("Bearer ")) {
-            return bearerToken.substring(7);
+    String resolveToken(HttpServletRequest req) {
+        String bearerToken = req.getHeader("Authorization");
+        if (bearerToken != null && bearerToken.startsWith("Bearer_")) {
+            return bearerToken.substring(7, bearerToken.length());
         }
         return null;
     }
@@ -68,13 +79,18 @@ public class JwtProvider {
         String login = getLogin(token);
 
         UserDetails userDetails = userDetailsService.loadUserByUsername(login);
-        User user = userService.findByEmail(login);
+        User user = userService.findByUserName(login);
 
         return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     }
 
     private String getLogin(String token) {
-        return Jwts.parser().setSigningKey(KEY).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody().getSubject();
+    }
+
+    @PostConstruct
+    protected void init() {
+        secret = Base64.getEncoder().encodeToString(KEY.getBytes());
     }
 
 }
