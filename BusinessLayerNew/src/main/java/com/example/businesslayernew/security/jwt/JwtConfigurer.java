6917123c4c8930b2eceb6d6e4f421291e90dd6ff
KatyaAvchinnikova <katsiaryna.auchynnikava@innowise-group>
2022-02-03 23:03:53 +0300

    Add JwtFilter

---
+++ BusinessLayerNew/src/main/java/com/example/businesslayernew/security/jwt/JwtConfigurer.java
@@ -0,0 +1,19 @@
+package com.example.businesslayernew.security.jwt;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+public class JwtConfigurer extends SecurityConfigurerAdapter<DefaultSecurityFilterChain, HttpSecurity> {
+
+    private JwtProvider jwtProvider;
+
+    @Override
+    public void configure(HttpSecurity httpSecurity) throws Exception {
+        JwtFilter jwtFilter = new JwtFilter(jwtProvider);
+        httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+    }
+}
--- BusinessLayerNew/src/main/java/com/example/businesslayernew/security/jwt/JwtFilter.java
+++ BusinessLayerNew/src/main/java/com/example/businesslayernew/security/jwt/JwtFilter.java
@@ -4,33 +4,34 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.GenericFilterBean;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@SuppressWarnings("NullableProblems")
+
 @RequiredArgsConstructor
 @Component
-public class JwtFilter extends OncePerRequestFilter {
+public class JwtFilter extends GenericFilterBean {
 
     private final JwtProvider jwtProvider;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request,
-                                    HttpServletResponse response,
-                                    FilterChain filterChain) throws ServletException, IOException {
-        String token = jwtProvider.resolveToken(request);
-
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String token = jwtProvider.resolveToken((HttpServletRequest) request);
         if (token != null && jwtProvider.validateToken(token)) {
-            Authentication authentication = jwtProvider.getAuthentication(token);
-            SecurityContextHolder.getContext().setAuthentication(authentication);
+            Authentication auth = jwtProvider.getAuthentication(token);
+
+            if (auth != null) {
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
         }
-
-        filterChain.doFilter(request, response);
+        chain.doFilter(request, response);
     }
-
 }
